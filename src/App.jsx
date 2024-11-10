70a30f9d75827dd087a1cf0c0d189d70a8d4637d
Sohel Rana <sohelrana130523@gmail.com>
2024-11-10 08:30:25 +0000

    Container folders structer and all the file added to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
 import './App.css'
+import { Article, Navbar, Cta, Brand, Feature } from './Components'
+import { Features, Footer, Header, Possibilities, Whatgpt, Blog } from './Containers'
 
 function App() {
 
   return (
     <>
+    <div>
+      <h1>Hasina Palabi kothai</h1>
+    </div>
     </>
   )
 }
